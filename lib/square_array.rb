2a3d5bedc50016d9c3ea6f9bae0c8ed18b58f8ec
Davis Brown <cooliomoded@gmail.com>
2020-08-07 02:18:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   i = 0
   squared_array = []
   while array.length > i do
-    squared_array.push(array[i].to_i ** 2)
+    squared_array.push(array[i] ** 2)
     i += 1
   end
   squared_array
